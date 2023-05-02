987ba7273d7703cc8c8df50d1aada1c4a3e222f3
RadheyAK47 <76564704+RadheyAK47@users.noreply.github.com>
2023-05-02 20:43:07 +0530

    Add files via upload

---
+++ same.py
@@ -0,0 +1,36 @@
+from tkinter import*
+from PIL import ImageTk,Image
+root=Tk()
+
+root.title("notebook")
+root.minsize(650,650)
+root.maxsize(650,650)
+
+open_img=ImageTk.PhotoImage(Image.open("open.png"))
+save_img=ImageTk.PhotoImage(Image.open("save.png"))
+close_img=ImageTk.PhotoImage(Image.open("exit.jpg"))
+
+label_filename=Label(root,text="File name")
+label_filename.place(relx=0.28,rely=0.03,anchor=CENTER)
+
+input_filename=Entry(root)
+input_filename.place(relx=0.46,rely=0.03,anchor=CENTER)
+
+my_text=Text(root,height=35,width=80)
+my_text.place(relx=0.5,rely=0.55,anchor=CENTER)
+
+def openfile():
+    print("opening file")
+    
+
+open_btn=Button(root,text="Open File",image=open_img,command=openfile)
+open_btn.place(relx=0.05,rely=0.03,anchor=CENTER)
+
+
+root.mainloop()
+
+
+
+
+
+
